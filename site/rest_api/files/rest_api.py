e602eb7fd2cb5b0e639b972686a45d7676927d12
tishbite <tishbite@futel.net>
2019-07-31 07:00:19 -0700

    pass api key via supervisor config. dummy api replaced.

--- site/rest_api/files/rest_api.py
+++ site/rest_api/files/rest_api.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
-from flask import Flask
+from flask import Flask, jsonify
+from datetime import datetime
+import requests
+import sqlite3
+import os
 
-app = Flask(__name__)
+api_key=os.environ['API_KEY']
 
-@app.route('/')
-def index():
-    return "Hello World!"
+def get_temp(key):
+    """ grab current temp in imperial and update sqlite DB """
+    owp = "https://api.openweathermap.org/data/2.5/weather?" \
+            "id=5746545&units=imperial&appid={}".format(key)
+    try:
+        req = requests.get(owp)
+    except requests.exceptions.RequestException as err:
+        print("Something went wrong: ", err)
+    return req.json()['main']['temp']
+
+api = Flask(__name__)
+
+@api.route('/temperature', methods=['GET'])
+def return_temp():
+    return jsonify(get_temp(api_key))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    api.run(host='0.0.0.0', port=5000)
